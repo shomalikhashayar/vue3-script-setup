b5c89c87b1bcfc0f29230cd9a89a86fb897205ab
Khashayar Shomali <khashi792017@gmail.com>
2023-02-08 17:18:44 +0330

    Create script setup with return

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>My name is {{ name }}, I'm {{age}} years old.</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "@vue/reactivity";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  setup() {
+    const name = ref("Khashayar Shomali");
+    const age = ref(22);
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    return {
+      name,
+      age,
+    };
+  },
+};
+</script>
