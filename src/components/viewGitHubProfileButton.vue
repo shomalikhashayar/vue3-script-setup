6cc6b4ecf42734b0f81be6a2e0a840380005ecfb
Khashayar Shomali <khashi792017@gmail.com>
2023-02-08 19:11:50 +0330

    Props

--- src/components/viewGitHubProfileButton.vue
+++ src/components/viewGitHubProfileButton.vue
@@ -1,12 +1,35 @@
 <template>
-  <button @click="openGitHubProfile">View My GitHub Profile</button>
+  <button @click="openGitHubProfile">{{ textWillShowOnButton }}</button>
 </template>
 
+<!-- script setup -->
 <script setup>
 const openGitHubProfile = () => {
   window.open("https://github.com/shomalikhashayar");
 };
 </script>
+
+<!-- composition api -->
+<!--
+<script>
+export default {
+  setup() {
+    const openGitHubProfile = () => {
+      window.open("https://github.com/shomalikhashayar");
+    };
+
+    return { openGitHubProfile };
+  },
+  props: {
+    textWillShowOnButton: {
+      type: String,
+      default: "View My GitHub Profile",
+    },
+  },
+};
+</script>
+-->
+
 <style>
 button {
   font-family: "Rubik", sans-serif;
