0f7648833c84c3f07a16e0a780cb58c785967634
Khashayar Shomali <khashi792017@gmail.com>
2023-02-08 18:18:24 +0330

    Methods in both of them (script with setup attribute - script with no setup attribute with return).

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,117 @@
 <template>
-  <h1>My name is {{ name }}, I'm {{ age }} years old.</h1>
+  <div class="khashayar-picture">
+    <a
+      href="http://github.com/shomalikhashayar"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src="../src/assets/khashayar.jpeg"
+        alt="Khashayar Shomali"
+        title="Khashayar Shomali"
+    /></a>
+  </div>
+  <div class="bio">
+    <h1 @mouseover="whenMouseOverHeading" @mouseout="whenMouseOutHeading">
+      {{ bio }}
+    </h1>
+  </div>
 </template>
 
 <script setup>
 import { ref } from "@vue/reactivity";
 
-const name = ref("Khashayar Shomali");
-const age = ref(22);
+const bio = ref("Hello, My name is Khashayar Shomali, I'm 22 years old.");
+
+const whenMouseOverHeading = () => {
+  bio.value =
+    "I'm a Software Engineer Who developing SPA websites with VUE.js ";
+};
+const whenMouseOutHeading = () => {
+  bio.value = "Hello, My name is Khashayar Shomali, I'm 22 years old.";
+};
 </script>
 
-<!-- script setup with return
+<!--
 <script>
 import { ref } from "@vue/reactivity";
 
 export default {
   setup() {
-    const name = ref("Khashayar Shomali");
-    const age = ref(22);
+    const bio = ref("Hello, My name is Khashayar Shomali, I'm 22 years old.");
+
+    const whenMouseOverHeading = () => {
+      bio.value =
+        "I'm a Software Engineer Who developing SPA websites with VUE.js ";
+    };
+
+    const whenMouseOutHeading = () => {
+      bio.value = "Hello, My name is Khashayar Shomali, I'm 22 years old.";
+    };
 
     return {
-      name,
-      age,
+      bio,
+      whenMouseOverHeading,
+      whenMouseOutHeading,
     };
   },
 };
 </script>
 -->
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Rubik&display=swap");
+
+body {
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+  background-color: bisque;
+}
+
+.bio {
+  height: 100px;
+  width: 900px;
+  background-color: blanchedalmond;
+  border-radius: 9px;
+  box-shadow: 0 4px 12px 0 rgba(255, 143, 15, 0.719);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+  font-family: "Rubik", sans-serif;
+  cursor: pointer;
+}
+
+@media screen and (max-width: 600px) {
+  body {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  .bio {
+    height: 100px;
+    width: 450px;
+    font-size: 10px;
+  }
+
+  .bio h1 {
+    max-width: 400px;
+    text-align: center;
+    line-height: 30px;
+  }
+}
+.khashayar-picture img {
+  border-radius: 50%;
+  width: 250px;
+  height: 250px;
+  border: 4px solid orange;
+  box-shadow: 0 4px 12px 0 rgba(255, 143, 15, 0.719);
+  cursor: pointer;
+}
+
+.khashayar-picture {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 50px;
+}
+</style>
